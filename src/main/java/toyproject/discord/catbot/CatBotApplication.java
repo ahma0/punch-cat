04aeaeb71ced7265826aa9ed802a32b7e8b87157
ahma0 <dsd932@naver.com>
2024-08-31 23:46:22 +0900

    chore: project setting

---
+++ src/main/java/toyproject/discord/catbot/CatBotApplication.java
@@ -0,0 +1,13 @@
+package toyproject.discord.catbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toyproject/discord/catbot/CatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.discord.catbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
