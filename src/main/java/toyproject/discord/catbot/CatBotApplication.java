6788bb1a6b55bc36bbb0c48e7f91027dc12c5687
ahma0 <dsd932@naver.com>
2024-09-01 23:58:27 +0900

    feat: main 코드 추가

--- src/main/java/toyproject/discord/catbot/CatBotApplication.java
+++ src/main/java/toyproject/discord/catbot/CatBotApplication.java
@@ -1,13 +1,31 @@
 package toyproject.discord.catbot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import toyproject.discord.catbot.domain.DiscordBotToken;
+import toyproject.discord.catbot.listener.CatBotListener;
+
+import javax.security.auth.login.LoginException;
 
 @SpringBootApplication
 public class CatBotApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CatBotApplication.class, args);
+    public static void main(String[] args) throws LoginException {
+        ApplicationContext context = SpringApplication.run(CatBotApplication.class, args);
+
+        DiscordBotToken discordBotTokenEntity = context.getBean(DiscordBotToken.class);
+        String discordBotToken = discordBotTokenEntity.getDiscordBotToken();
+
+        JDA jda = JDABuilder.createDefault(discordBotToken)
+                .setActivity(Activity.playing("메시지 대기 중"))
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .addEventListeners(new CatBotListener())
+                .build();
     }
 
 }
